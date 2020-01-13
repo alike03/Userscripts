6e64869c47b233d495fef28018e54278a1f95185
alike03 <alikemal03@live.at>
2020-01-13 23:32:02 +0100

    YouTube Docking: player increase; MHW Event Log: added

--- meta/YouTubeDocking.user.js
+++ meta/YouTubeDocking.user.js
@@ -2,9 +2,9 @@
 // @name        YouTube Docking
 // @description Read the comments while watching the video
 // @author      alike03
-// @version     2.0.1
+// @version     2.0.2
 // @namespace   youtubeDOCK
-// @icon        https://raw.githubusercontent.com/alike03/Userscripts/master/assets/youtube.png
+// @icon        https://raw.githubusercontent.com/alike03/Userscripts/master/assets/YouTubeDocking-Icon.png
 // @supportURL  https://github.com/alike03/Userscripts/issues
 // @downloadURL https://raw.githubusercontent.com/alike03/Userscripts/master/src/YouTubeDocking.user.js
 // @updateURL   https://raw.githubusercontent.com/alike03/Userscripts/master/meta/YouTubeDocking.user.js
